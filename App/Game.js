d9900fd49a8390f93ec01863c2f9da92b893cca3
CodingTigerPaw <codingtigerpaw@gmail.com>
2020-09-24 08:22:30 +0100

    Created new project Game set basics structer and Game object

---
+++ App/Game.js
@@ -0,0 +1,17 @@
+class Game {
+    constructor({
+        letterWrapper,
+        categoryWrapper,
+        wordWrapper,
+        outputWrapper
+    }){
+        this.letterWrapper = letterWrapper;
+        this. categoryWrapper = categoryWrapper;
+        this.wordWrapper = wordWrapper;
+        this.outputWrapper = outputWrapper
+    }
+    init(){
+        
+    }   
+}
+
