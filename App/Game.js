b5ad892fcb9cabd38a607faaae66fde14fea796f
CodingTigerPaw <codingtigerpaw@gmail.com>
2020-09-24 11:38:11 +0100

    continue working on a class Game and create file for Class Quote

--- App/Game.js
+++ App/Game.js
@@ -6,12 +6,46 @@ class Game {
         outputWrapper
     }){
         this.letterWrapper = letterWrapper;
-        this. categoryWrapper = categoryWrapper;
+        this.categoryWrapper = categoryWrapper;
         this.wordWrapper = wordWrapper;
         this.outputWrapper = outputWrapper
+
+        const {text , category} = this.quotes[Math.floor(Math.random() * this.quotes.length)]
+        this.categoryWrapper.innerHTML = category
     }
-    init(){
-        
+
+    quotes = [{
+        text: 'tygrys',
+        category: "duży kot"
+    },
+    {
+        text: 'koczilla',
+        category: 'zwierzę domowę'
+    },
+    {
+        text: "rekin",
+        category: 'żyje w wodzie'
+    }]
+    showLabel(letter){
+        console.log(letter)
+       }
+    drawLetters(){
+        for(let i=0; i<26; i++){
+            const label = (i+10).toString(36)  // crate alfabet labels from asci codes
+            const button = document.createElement('button')
+            button.classList.add('btn')
+            button.addEventListener('click', () => this.showLabel(label))  //
+            button.innerHTML = label
+            this.letterWrapper.appendChild(button)
+        }
+    }
+    selectQoute(){
+
     }   
+
+    init(){
+       this.drawLetters()
+    }
+
 }
 
